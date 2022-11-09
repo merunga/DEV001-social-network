db9a5da9954f16e3bba22faf1e9a93806e52783c
merunga <merunga@gmail.com>
2022-11-09 11:33:40 -0500

    quitar ciclos de imports

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
-// eslint-disable-next-line import/no-cycle
 import { Home } from './component/Home.js';
-// eslint-disable-next-line import/no-cycle
 import { Register } from './component/Register.js';
-// eslint-disable-next-line import/no-cycle
 import { Login } from './component/Login.js';
 
 const rootDiv = document.getElementById('root');
 
-const routes = {
-  '/': Home,
-  '/register': Register,
-  '/login': Login,
-};
-export const onNavigate = (pathname) => {
+let routes = {};
+
+const onNavigate = (pathname) => {
   window.history.pushState(
     {},
     pathname,
@@ -22,8 +16,15 @@ export const onNavigate = (pathname) => {
     rootDiv.removeChild(rootDiv.firstChild);
   }
 
-  rootDiv.appendChild(routes[pathname]());
+  rootDiv.appendChild(routes[pathname]);
 };
+
+routes = {
+  '/': Home(onNavigate),
+  '/register': Register(onNavigate),
+  '/login': Login(onNavigate),
+};
+
 const component = routes[window.location.pathname];
 
 window.onpopstate = () => {
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Home } from './component/Home.js';
+import { Register } from './component/Register.js';
+import { Login } from './component/Login.js';
+
+export const routes = {
+  '/': Home,
+  '/register': Register,
+  '/login': Login,
+};
