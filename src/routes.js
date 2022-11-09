7f66bf06a8e361fffc17f3e000d467a0cdc33e95
merunga <merunga@gmail.com>
2022-11-09 11:40:25 -0500

    quitar ciclos de imports

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import { Home } from './component/Home.js';
-import { Register } from './component/Register.js';
-import { Login } from './component/Login.js';
-
-export const routes = {
-  '/': Home,
-  '/register': Register,
-  '/login': Login,
-};
